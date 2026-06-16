2f4b95d5fe3bae6dce076d7a2afb6c1d228ad92b
Deo <deotri4@gmail.com>
2026-06-16 22:51:13 +0800

    Add layout structure with header and footer components; update metadata and home page content

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    browserToTerminal: true,
+    // 'error' - errors only (default)
+    // 'warn' - warnings and errors
+    // 'info' - informational messages, warnings, and errors
+    // 'true' - all console output
+    // 'fatal' - disabled
+  },
 };
 
 export default nextConfig;
